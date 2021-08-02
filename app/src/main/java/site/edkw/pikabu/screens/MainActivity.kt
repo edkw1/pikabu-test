1975f5fcb26741c0d5126888d3157d4d558b01ef
Azat <edkw@ya.ru>
2021-08-02 09:32:50 +0500

    removed unused imports

--- app/src/main/java/site/edkw/pikabu/screens/MainActivity.kt
+++ app/src/main/java/site/edkw/pikabu/screens/MainActivity.kt
@@ -1,16 +1,9 @@
 package site.edkw.pikabu.screens
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import site.edkw.pikabu.R
-import site.edkw.pikabu.viewmodels.PostsListViewModel
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
